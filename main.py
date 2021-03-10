ab5e7b4049f2911a840fc0187475ce1874ec2b65
ds6ga6 <ruanfanjie821@gmail.com>
2021-03-10 16:30:29 +0800

    ✨feat(new t_to_v.py): 改#为region

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-import os
+'''
+参考：https://www.runoob.com/python/python-gui-tkinter.html
+'''
 
+# Python2.x 导入方法
+from tkinter import *           # 导入 Tkinter 库
+# Python3.x 导入方法
+#from tkinter import *
+root = Tk()                     # 创建窗口对象的背景色
+# 创建两个列表
+li = ['C', 'python', 'php', 'html', 'SQL', 'java']
+
+listb = Listbox(root)  # 创建两个列表组件
+
+for item in li:                 # 第一个小部件插入数据
+    listb.insert(0, item)
+
+
+listb.pack()                    # 将小部件放置到主窗口中
+
+root.mainloop()                 # 进入消息循环
