58b017c2fb5467a4aa05ee66e06d56099f9f410b
ds6ga6 <ruanfanjie821@gmail.com>
2021-03-14 20:33:40 +0800

    🦄refactor(haha.py): 用ENDLABEL替代‘【END】’

--- haha.py
+++ haha.py
@@ -17,7 +17,7 @@ if(os.path.exists(FILENAME)):
 	os.system('copy ' + FILENAME + ' .' + FILENAME)
 else:
 	with open(FILENAME, 'w', encoding='utf-8') as f:
-		f.write('\n\n【END】')
+		f.write('\n\n'+ENDLABEL)
 
 ################################################################################
 # 打开 FILENAME 并在 ENDLABEL 前写入时间
@@ -34,7 +34,7 @@ time = '@data:' + datastring + '\n'
 # 分隔符
 sech = '\n---\n\n'
 
-fdata = fdata.replace('\n\n【END】', '\n\n'+sech+time+sech+'\n\n【END】')
+fdata = fdata.replace('\n\n'+ENDLABEL, '\n\n'+sech+time+sech+'\n\n'+ENDLABEL)
 
 with open(FILENAME, 'w', encoding='utf-8') as f:
 	f.write(fdata)
