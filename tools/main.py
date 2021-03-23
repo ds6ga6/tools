575262cdf60c41116a1edd740b969f03b1816887
ds6ga6 <ruanfanjie821@gmail.com>
2021-03-23 16:08:20 +0800

    📃docs(main.py): 增加注释，为下一步的重构准备

--- tools/main.py
+++ tools/main.py
@@ -21,23 +21,33 @@ args = {
 	"pathSeparator"				: sys.argv[12]	#	/ on macOS or linux, \\ on Windows
 }
 
+# 读取登记过的命令列表，保存为以下形式：
+# commandDict = {'commandName1':'filename1', 'commandName2':'filename2'}
 with open(REGISTER_FILELOAD, 'r', encoding='utf-8') as f:
 	data = json.load(f)
 commandDict = {}
 for commandName in data.keys():
 	commandDict[commandName] = data[commandName]['filename']
 
-def exec_command(command):
-	commandsplit = command.split(' ', 1)
-	if(commandsplit[0] in commandDict.keys()):
-		new_command = 'python ' + CODE_LOAD + commandDict[commandsplit[0]] + ' ' + commandsplit[1]
-		print(new_command)
-		os.system(new_command)
+# 执行命令
+# commandType 代表要执行的命令的类型，比如 cmd, python, user(自定义的), ... 具体代码中会说明
+# 如果类型是 'auto'。1. user; 2. cmd
+def exec_command(command, commandType='auto'):
+	if(commandType=='auto'):
+		commandsplit = command.split(' ', 1)	# 取command最前面的字符串
+		if(commandsplit[0] in commandDict.keys()):	# 成立代表是自定义的命令
+			# 默认 python, 要用非 python 的要改这里
+			new_command = 'python ' + CODE_LOAD + commandDict[commandsplit[0]] + ' ' + commandsplit[1]
+			os.system(new_command)
+		else:
+			os.system(command)	# BUG 这里没有对命令是否是 cmd 命令做判断
 	else:
-		os.system(command)
+		pass
+	
 
 # 对 main.md
 if(args['relativeFile']=='main.md'):
+	# 打开命令，并读取对应的行
 	with open(args['relativeFile'], 'r', encoding='utf-8') as f:
 		command = f.readlines()[args['lineNumber']-1]
 	exec_command(command)
